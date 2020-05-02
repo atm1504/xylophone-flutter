28fb8c29f09f79d758b339bd3144e4a14dc3e6f6
Amartya Mondal <hayyoulistentome@gmail.com>
2020-05-02 23:40:14 +0530

    feat: Finalize the project

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # xylophoneflutter
 
 A flutter app to learn dependency integration.
+Here I have used audioplayers dependency to play music on click of buttons.
+
+<img width="515" alt="Screenshot 2020-05-02 at 11 39 35 PM" src="https://user-images.githubusercontent.com/43731599/80872218-32b38980-8cce-11ea-8a56-393ae69c0500.png">
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,18 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  Expanded buildKey({Color color, int positionNumber, String text}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(positionNumber);
+        },
+        child: Text(text),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,69 +35,13 @@ class XylophoneApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.pink,
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Text('Click here'),
-                ),
-              ),
+              buildKey(color: Colors.red, positionNumber: 1, text: "sa"),
+              buildKey(color: Colors.orange, positionNumber: 2, text: "re"),
+              buildKey(color: Colors.yellow, positionNumber: 3, text: "ga"),
+              buildKey(color: Colors.green, positionNumber: 4, text: "ma"),
+              buildKey(color: Colors.blue, positionNumber: 5, text: "pa"),
+              buildKey(color: Colors.indigo, positionNumber: 6, text: "dha"),
+              buildKey(color: Colors.purple, positionNumber: 7, text: "ni"),
             ],
           ),
         ),
